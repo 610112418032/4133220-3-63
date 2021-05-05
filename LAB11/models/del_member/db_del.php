7a9c49747cb72598472dbbdd75b2f737249005b9
610112418032 <78150850+610112418032@users.noreply.github.com>
2021-05-05 12:10:52 +0700

    Add files via upload

---
+++ LAB11/models/del_member/db_del.php
@@ -0,0 +1,19 @@
+<?php
+require "../conDB.php";
+
+$id =$_GET["id"];
+
+try{
+    $SQL = "DELETE FROM MEMBER
+            WHERE member_id = '$id'
+            ";
+    $conn->query($SQL);
+
+
+}
+catch(Exception $e){
+    echo $e->getMexxage();
+}
+
+
+?>
